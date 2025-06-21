9c8efd2bf41b53b7d0b93a4d74f4a220ba813705
tianchang <tianchang488@gmail.com>
2025-06-22 01:09:32 +0800

    feat:测试push操作

--- src/view/Overview.vue
+++ src/view/Overview.vue
@@ -368,7 +368,7 @@ onMounted(() => {
               </svg>
               <span class="text-sm">导入仓库</span>
             </Button>
-            <Button variant="outline" class="h-20 flex-col space-y-2">
+            <Button variant="outline" class="h-20 flex-col space-y-2 cursor-pointer">
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
               </svg>
